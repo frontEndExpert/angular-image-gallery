d8490812f2409d4609c19a8e150ccf7666328536
frontEndExpert <aylon.spigel@gmail.com>
2018-04-04 08:53:56 +0700

    step 2

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
 import {  Component, OnInit, EventEmitter, Input, Output,
-          ChangeDetectorRef,ChangeDetectionStrategy, IterableDiffers ,IterableDiffer } from '@angular/core';
+          ChangeDetectorRef,ChangeDetectionStrategy, 
+          IterableDiffers, IterableDiffer } from '@angular/core';
 import {LocalStorageService} from '../image-local-storage.service';
 import { ModalsService, ModalStateParam, ModalType } from '../modals.service';
 
@@ -11,7 +12,7 @@ import { ModalsService, ModalStateParam, ModalType } from '../modals.service';
 export class GalleryComponent implements OnInit{
   galleryObject: any = this.ls.getImageGallery();
   numberOfImages: number = (this.galleryObject)?this.galleryObject.length:0;
-  differ: IterableDiffer;
+  differ: IterableDiffer<any>;
 
   constructor(
     private _modalsService: ModalsService,
@@ -21,8 +22,9 @@ export class GalleryComponent implements OnInit{
       this.differ = differs.find([]).create(null);
     }
 
-  
+  url: any = "";
 
+  imgIndex: number= 0;
   currentPage = 1; // the page being displayed by the pagination PIPE
   maxSize: number = 10; // the number of cars/items display on 1 page
 
@@ -30,7 +32,7 @@ export class GalleryComponent implements OnInit{
     this.galleryObject = this.ls.getImageGallery();
     this.numberOfImages = (this.galleryObject)?this.galleryObject.length:0;
     
-    this.ref.markForCheck();
+    //this.ref.markForCheck();
     console.log("numberOfImages:", JSON.stringify(this.numberOfImages))
   }
   
@@ -50,12 +52,36 @@ export class GalleryComponent implements OnInit{
    // this.displaywin.emit(id);
   }
   ngDoCheck() {
-	  const changes = this.differ.diff(this.galleryObject);
+    this.galleryObject=this.ls.getImageGallery();
+    const changes = this.differ.diff(this.galleryObject);
 	  if (changes) {
 	    console.log('new change');// for splitting up changes
       changes.forEachAddedItem(r => console.log('added ', r));
       changes.forEachRemovedItem(r => console.log('removed ', r))
       changes.forEachMovedItem(r => console.log('moved ', r))
     }
-	}
+  }
+  
+
+  
+  addImageToGallery(){
+    this.ls.addObjectToImageGallery(this.url);
+    //this.ref.markForCheck();private ref: ChangeDetectorRef
+    //this.hide();
+  }
+
+  deleteAllImages(){
+    this.ls.deleteAllImages();
+  }
+
+  readUrl(event:any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.onload = (event:any) => {
+        this.url = event.target.result;
+      }
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
 }
